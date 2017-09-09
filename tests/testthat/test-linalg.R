7c7724665f5320abe34bf4309dd4c9a6dd2296ae
Joshua Loyal <jloyal25@gmail.com>
2017-09-09 12:09:32 -0500

    added tests for linalg

---
+++ tests/testthat/test-linalg.R
@@ -0,0 +1,33 @@
+library(expm)
+
+test_that('Sample Covariance', {
+  set.seed(123)
+
+  n_samples <- 100
+  n_features <- 4
+
+  X <- matrix(rnorm(n_samples * n_features), nrow = n_samples, ncol = n_features)
+  sigma <- sample_covariance(X)
+
+  # we can calculate the biased estimate from the unbiased estimate
+  # by changing the scale factor.
+  expected_sigma <- (n_samples - 1) / n_samples * cov(X)
+
+  expect_true(isSymmetric(sigma))
+  expect_equal(sigma, expected_sigma)
+})
+
+test_that('Matrix Square Root', {
+  set.seed(123)
+
+  n_samples <- 100
+  n_features <- 4
+
+  X <- matrix(rnorm(n_samples * n_features), nrow = n_samples, ncol = n_features)
+  sigma <- sample_covariance(X)
+
+  sigma_sqrt <- matrix_sqrt(sigma)
+  expected_sqrt <- sqrtm(sigma)
+
+  expect_equal(sigma_sqrt, expected_sqrt)
+})
